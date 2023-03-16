8cca02d17dc2f2337b9d299b8992141246c39300
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2023-03-16 15:00:01 +0600

    Checkout - issues - fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,4 +37,38 @@ $(document).ready(function () {
 });
 
 
+/* Home page start  */
+
+// home video
+
+new YouTubeToHtml5({ withAudio: true });
+
+// home video button
+
+(function ($) {
+    $(document).ready(function () {
+        $diviVideo = $("#home-video video");
+        videoElement = $("#home-video video")[0];
+        $videoCta = $(".home-buy-button");
+
+        $diviVideo.on("timeupdate", function (e) {
+            if (e.target.currentTime >= 5) {
+                $videoCta.addClass("show-button");
+            }
+
+        });
+    }); 
+})(jQuery);
+
+
+
+
+$('.btn-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
 
